311a212075e6a7d8db154cb914d791a68bfe2aaf
JihoonKim1004 <JihoonKim1004@a4109968-4c52-0410-a50f-f9502a57239f>
2008-09-24 06:07:01 +0000

    Modified the code to create MXMLRenderKitFactoryImplWrapper.java file during build time for different implementation [i.e. MyFaces or Mojarra]. Thinking whether to use composition design pattern for this as done with an another class and loading the implementation file during runtime, will consider the change over the weekend.

---
+++ trunk/jsf-flex-build-plugIn/jsf-flex-plugIn/src/main/java/com/googlecode/jsfFlexPlugIn/mojo/CreateMXMLRenderKitFactoryImplWrapperJavaFileMojo.java
@@ -0,0 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.googlecode.jsfFlexPlugIn.mojo;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import com.googlecode.jsfFlexPlugIn.parser._JsfFlexParserListener;
+import com.googlecode.jsfFlexPlugIn.parser.velocity.JsfFlexVelocityParser;
+
+/**
+ * @goal    createMXMLRenderKitFactoryImplWrapperJavaFile
+ * @phase   generate-sources
+ * @author Ji Hoon Kim
+ */
+public class CreateMXMLRenderKitFactoryImplWrapperJavaFileMojo extends AbstractMojo 
+															   implements _JsfFlexParserListener {
+	
+	private static final String MXML_RENDER_KIT_FACTORY_IMPL_WRAPPER_BASE_IMPL = "mxmlRenderKitFactoryImplWrapperBaseImpl";
+	
+	private static final String JSF_FLEX_MXML_RENDER_KIT_FACTORY_IMPL_WRAPPER_BASE_IMPL_TEMPLATE = "MXMLRenderKitFactoryImplWrapper.vm";
+	private static final String TO_CREATE_JSF_FLEX_MXML_RENDER_KIT_FACTORY_IMPL_WRAPPER_BASE_IMPL_FILE_NAME = "MXMLRenderKitFactoryImplWrapper.java";
+	
+	private static final String FILE_RESOURCE_LOADER_PATH_KEY = "file.resource.loader.path";
+	
+	/**
+     * @parameter expression="${mxmlRenderKitFactoryImplWrapperBaseImpl}"
+     */
+	private String _mxmlRenderKitFactoryImplWrapperBaseImpl;
+	
+	/**
+     * @parameter expression="${basedir}/target/jsfFlex-builder-plugin/main/java/com/googlecode/jsfFlex/renderkit"
+     */
+	private File _toCreateMXMLRenderKitFactoryImplWrapperBaseImplPath;
+	
+	/**
+     * @parameter expression="src/main/resources/META-INF"
+     */
+    private File _templateSourceDirectory;
+    
+    private JsfFlexVelocityParser _jsfFlexVelocityParser;
+	
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		
+		Properties _velocityParserProperties = new Properties();
+		_velocityParserProperties.put(FILE_RESOURCE_LOADER_PATH_KEY, _templateSourceDirectory.getPath());
+		
+		_jsfFlexVelocityParser = new JsfFlexVelocityParser(_velocityParserProperties);
+		_jsfFlexVelocityParser.init();
+		_jsfFlexVelocityParser.addParserListener(this);
+		
+		String toCreateMXMLRenderKitFactoryImplWrapperBaseImplJavaPath = _toCreateMXMLRenderKitFactoryImplWrapperBaseImplPath.getPath();
+		
+		try{
+			File _toCreateMXMLRenderKitFactoryImplWrapperBaseImplFilePath = new File(toCreateMXMLRenderKitFactoryImplWrapperBaseImplJavaPath);
+			if(!_toCreateMXMLRenderKitFactoryImplWrapperBaseImplFilePath.exists()){
+				_toCreateMXMLRenderKitFactoryImplWrapperBaseImplFilePath.mkdirs();
+			}
+			toCreateMXMLRenderKitFactoryImplWrapperBaseImplJavaPath +=  File.separatorChar + 
+																			TO_CREATE_JSF_FLEX_MXML_RENDER_KIT_FACTORY_IMPL_WRAPPER_BASE_IMPL_FILE_NAME;
+			FileWriter _writer = new FileWriter(new File(toCreateMXMLRenderKitFactoryImplWrapperBaseImplJavaPath));
+			Map<String, Object> _contextInfoMap = new HashMap<String, Object>();
+			
+			_contextInfoMap.put(MXML_RENDER_KIT_FACTORY_IMPL_WRAPPER_BASE_IMPL, _mxmlRenderKitFactoryImplWrapperBaseImpl);
+			_jsfFlexVelocityParser.mergeCollectionToTemplate(JSF_FLEX_MXML_RENDER_KIT_FACTORY_IMPL_WRAPPER_BASE_IMPL_TEMPLATE, _contextInfoMap, 
+																_writer, toCreateMXMLRenderKitFactoryImplWrapperBaseImplJavaPath);
+			
+		}catch(IOException _ioException){
+			
+		}
+		
+	}
+	
+	public void mergeCollectionToTemplateFinished(String _fileMerged) {
+		
+	}
+	
+}
