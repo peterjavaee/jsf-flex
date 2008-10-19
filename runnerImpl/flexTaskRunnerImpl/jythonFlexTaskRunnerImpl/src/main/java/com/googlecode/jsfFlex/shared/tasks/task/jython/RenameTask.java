b951e9bf24cc5eebf30c8eac2bf00479e49a4a76
JihoonKim1004 <JihoonKim1004@a4109968-4c52-0410-a50f-f9502a57239f>
2008-10-19 23:45:31 +0000

    Initial check-in of Jython impl of _FlexTaskRunner. It has yet to be tested and when Jython 2.5 is publicly released with it's resource provided within maven repo, it will be tested then. In order to use this impl [which will be used only during debugMode of the RenderKit] PYTHON_HOME must be set to the correct path and can be set within the web.xml as python.home init parameter or can be passed as JVM python.home parameter. Will delete the branch for this change as well [is it me or does Google Code have SVN < 1.5 version, since couldn't do a traditional merge --reintegrate].

---
+++ runnerImpl/flexTaskRunnerImpl/jythonFlexTaskRunnerImpl/src/main/java/com/googlecode/jsfFlex/shared/tasks/task/jython/RenameTask.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.googlecode.jsfFlex.shared.tasks.task.jython;
+
+import org.python.core.PyInteger;
+import org.python.core.PyObject;
+import org.python.core.PyString;
+import org.python.util.PythonInterpreter;
+
+/**
+ * @author Ji Hoon Kim
+ */
+public final class RenameTask extends _JythonBaseTask {
+	
+	private static final String PYTHON_EXECUTION_FILE = "renameTask.py";
+	
+	private static final PyObject _renameTaskClass;
+	
+	static{
+		PythonInterpreter interpreter = new PythonInterpreter();
+		interpreter.execfile(RenameTask.class.getResourceAsStream(PYTHON_EXECUTION_FILE));
+		_renameTaskClass = interpreter.get("RenameTask");
+	}
+	
+	private String _sourceFile;
+	private String _destFile;
+	private boolean _overwrite;
+	
+	public RenameTask(){
+		super();
+	}
+	
+	public RenameTask(String sourceFile, String destFile, boolean overwrite){
+		super();
+		_sourceFile = sourceFile;
+		_destFile = destFile;
+		_overwrite = overwrite;
+	}
+	
+	void build() {
+		
+		PyObject _renameTaskObject = _renameTaskClass.__call__(new PyString(_sourceFile), new PyString(_destFile), 
+																	new PyInteger(_overwrite ? 1 : 0));
+		_jythonTask = (_JythonTaskPerformer) _renameTaskObject.__tojava__(_JythonTaskPerformer.class);
+	}
+	
+	public String toString() {
+		StringBuffer content = new StringBuffer();
+		content.append("sourceFile [ ");
+		content.append(_sourceFile);
+		content.append(" ] ");
+		content.append("destFile [ ");
+		content.append(_destFile);
+		content.append(" ] ");
+		content.append("overwrite [ ");
+		content.append(_overwrite);
+		content.append(" ] ");
+		
+		return content.toString();
+	}
+	
+	public void destFile(String destFile) {
+		_destFile = destFile;
+	}
+	public void overwrite(boolean overwrite) {
+		_overwrite = overwrite;
+	}
+	public void sourceFile(String sourceFile) {
+		_sourceFile = sourceFile;
+	}
+	
+}
