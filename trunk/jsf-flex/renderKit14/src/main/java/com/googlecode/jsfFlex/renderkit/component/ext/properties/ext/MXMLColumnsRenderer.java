5d1a42970c78512b7c81fd6b84e5e2e443099b32
JihoonKim1004 <JihoonKim1004@a4109968-4c52-0410-a50f-f9502a57239f>
2009-02-21 20:41:44 +0000

    Made the change for DataGrid component, s.t. if dataBinding is not used ActionScript content will not be included.

--- trunk/jsf-flex/renderKit14/src/main/java/com/googlecode/jsfFlex/renderkit/component/ext/properties/ext/MXMLColumnsRenderer.java
+++ trunk/jsf-flex/renderKit14/src/main/java/com/googlecode/jsfFlex/renderkit/component/ext/properties/ext/MXMLColumnsRenderer.java
@@ -71,6 +71,10 @@ public final class MXMLColumnsRenderer extends MXMLComponentBaseRenderer {
 		}
 		
 		AbstractMXMLUIDataGrid dataGrid = (AbstractMXMLUIDataGrid) dataGridComponent;
+		if(dataGrid.getBindingBeanList() == null){
+			return;
+		}
+		
 		Integer batchColumnDataRetrievalSize = Integer.valueOf(dataGrid.getBatchColumnDataRetrievalSize());
 		String rowCount = dataGrid.getRowCount();
 		
