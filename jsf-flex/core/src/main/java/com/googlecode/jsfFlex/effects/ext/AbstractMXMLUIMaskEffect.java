d3c45fd053b2b310b36c379f1de8adb748f1b31e
JihoonKim1004 <JihoonKim1004@gmail.com>
2009-10-16 05:07:54 +0000

    Created several components within the effects package : AddItemAction, AnimateProperty, Blur, DefaultListEffect, DefaultTileListEffect, Dissolve, Fade, Glow, Iris, MaskEffect, Paraller, TweenEffect. After all components have been added, need to bring integration components between one another.

---
+++ jsf-flex/core/src/main/java/com/googlecode/jsfFlex/effects/ext/AbstractMXMLUIMaskEffect.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.googlecode.jsfFlex.effects.ext;
+
+import org.apache.myfaces.buildtools.maven2.plugin.builder.annotation.JSFComponent;
+
+import com.googlecode.jsfFlex.component.MXMLUISimpleBase;
+
+/**
+ * @author Ji Hoon Kim
+ */
+@JSFComponent(
+        name                =   "jf:mxmlMaskEffect",
+        clazz               =   "com.googlecode.jsfFlex.effects.ext.MXMLUIMaskEffect",
+        type                =   "com.googlecode.jsfFlex.MXMLUIMaskEffect",
+        tagClass            =   "com.googlecode.jsfFlex.taglib.ext.MXMLUIMaskEffectTag",
+        family              =   "javax.faces.MXMLSimple",
+        defaultRendererType =   "com.googlecode.jsfFlex.MXMLMaskEffect"
+)
+public abstract class AbstractMXMLUIMaskEffect 
+                                extends MXMLUISimpleBase 
+                                implements _MXMLUIMaskEffectAttributes {
+    
+}
---
+++ jsf-flex/renderKit15/src/main/java/com/googlecode/jsfFlex/renderkit/effects/ext/MXMLTweenEffectRenderer.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.googlecode.jsfFlex.renderkit.effects.ext;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import org.apache.myfaces.buildtools.maven2.plugin.builder.annotation.JSFRenderer;
+
+import com.googlecode.jsfFlex.renderkit.annotation.JsfFlexAttributeProperties;
+import com.googlecode.jsfFlex.renderkit.effects.MXMLTweenEffectTemplateRenderer;
+import com.googlecode.jsfFlex.renderkit.mxml.AbstractMXMLResponseWriter;
+import com.googlecode.jsfFlex.shared.adapter._MXMLContract;
+
+/**
+ * @author Ji Hoon Kim
+ */
+@JSFRenderer(
+        renderKitId="MXML_BASIC",
+        family="javax.faces.MXMLSimple",
+        type="com.googlecode.jsfFlex.MXMLTweenEffect"
+)
+@JsfFlexAttributeProperties(
+        mxmlComponentName="TweenEffect",
+        mxmlComponentNodeAttributes={},
+
+        jsfFlexAttributes={}
+)
+public final class MXMLTweenEffectRenderer extends MXMLTweenEffectTemplateRenderer {
+
+    @Override
+    public void encodeBegin(FacesContext context, UIComponent componentObj) throws IOException {
+        super.encodeBegin(context, componentObj);
+        
+        _MXMLContract componentMXML = (_MXMLContract) componentObj;
+        
+        AbstractMXMLResponseWriter writer = (AbstractMXMLResponseWriter) context.getResponseWriter();
+        writer.createPreMxml(componentMXML, MXMLTweenEffectRenderer.class.getAnnotation(JsfFlexAttributeProperties.class).mxmlComponentName(), 
+                null);
+        
+    }
+    
+}
