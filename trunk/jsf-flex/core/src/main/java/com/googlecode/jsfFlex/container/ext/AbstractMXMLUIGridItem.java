5c18f0b3a077d33d95d1d00eb5e1a0ea0715f58f
JihoonKim1004@gmail.com <JihoonKim1004@gmail.com@a4109968-4c52-0410-a50f-f9502a57239f>
2009-10-20 04:16:27 +0000

    Moved the components from one package to an another for better packaging.

---
+++ trunk/jsf-flex/core/src/main/java/com/googlecode/jsfFlex/container/ext/AbstractMXMLUIGridItem.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.googlecode.jsfFlex.container.ext;
+
+import java.io.IOException;
+
+import javax.faces.context.FacesContext;
+
+import org.apache.myfaces.buildtools.maven2.plugin.builder.annotation.JSFComponent;
+
+import com.googlecode.jsfFlex.attributes._MXMLUIColSpanAttribute;
+import com.googlecode.jsfFlex.attributes._MXMLUIRowSpanAttribute;
+import com.googlecode.jsfFlex.component.MXMLUISimpleBase;
+
+/**
+ * @author Ji Hoon Kim
+ */
+@JSFComponent(
+        name                =   "jf:mxmlGridItem",
+        clazz               =   "com.googlecode.jsfFlex.container.ext.MXMLUIGridItem",
+        type                =   "com.googlecode.jsfFlex.MXMLUIGridItem",
+        tagClass            =   "com.googlecode.jsfFlex.taglib.ext.MXMLUIGridItemTag",
+        family              =   "javax.faces.MXMLSimple",
+        defaultRendererType =   "com.googlecode.jsfFlex.MXMLGridItem"
+)
+public abstract class AbstractMXMLUIGridItem 
+                        extends MXMLUISimpleBase 
+                        implements _MXMLUIBoxAttributes, _MXMLUIRowSpanAttribute, _MXMLUIColSpanAttribute {
+    
+    public void encodeBegin(FacesContext context) throws IOException {
+        /*
+         * Grid item will have direction set to horizontal
+         */
+        getAttributes().put("direction", "horizontal");
+        
+        super.encodeBegin(context);
+    }
+    
+}
---
+++ trunk/jsf-flex/renderKit15/src/main/java/com/googlecode/jsfFlex/renderkit/container/ext/MXMLGridRowRenderer.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.googlecode.jsfFlex.renderkit.container.ext;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import org.apache.myfaces.buildtools.maven2.plugin.builder.annotation.JSFRenderer;
+
+import com.googlecode.jsfFlex.renderkit.annotation.JsfFlexAttributeProperties;
+import com.googlecode.jsfFlex.renderkit.container.MXMLBoxTemplateRenderer;
+import com.googlecode.jsfFlex.renderkit.mxml.AbstractMXMLResponseWriter;
+import com.googlecode.jsfFlex.shared.adapter._MXMLContract;
+
+/**
+ * @author Ji Hoon Kim
+ */
+@JSFRenderer(
+        renderKitId="MXML_BASIC",
+        family="javax.faces.MXMLSimple",
+        type="com.googlecode.jsfFlex.MXMLGridRow"
+)
+@JsfFlexAttributeProperties(
+        mxmlComponentName="GridRow",
+        mxmlComponentNodeAttributes={},
+
+        jsfFlexAttributes={}
+)
+public final class MXMLGridRowRenderer extends MXMLBoxTemplateRenderer {
+    
+    @Override
+    public void encodeBegin(FacesContext context, UIComponent componentObj) throws IOException {
+        super.encodeBegin(context, componentObj);
+        
+        _MXMLContract componentMXML = (_MXMLContract) componentObj;
+        
+        AbstractMXMLResponseWriter writer = (AbstractMXMLResponseWriter) context.getResponseWriter();
+        writer.createPreMxml(componentMXML, MXMLGridRowRenderer.class.getAnnotation(JsfFlexAttributeProperties.class).mxmlComponentName(), 
+                null);
+        
+    }
+    
+}
