b47975624357c462dca6b5c2d11756008d20e754
JihoonKim1004@gmail.com <JihoonKim1004@gmail.com@a4109968-4c52-0410-a50f-f9502a57239f>
2010-08-01 04:43:23 +0000

    Added remaining effects components.

---
+++ branches/release-1.1b/jsf-flex/core/src/main/java/com/googlecode/jsfFlex/effects/ext/AbstractFlexUIScale3D.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.googlecode.jsfFlex.effects.ext;
+
+import org.apache.myfaces.buildtools.maven2.plugin.builder.annotation.JSFComponent;
+import org.apache.myfaces.buildtools.maven2.plugin.builder.annotation.JSFProperty;
+
+import com.googlecode.jsfFlex.attributes.IFlexUIBaseAttributes;
+import com.googlecode.jsfFlex.component.AbstractFlexUISimpleBase;
+
+/**
+ * @author Ji Hoon Kim
+ */
+@JSFComponent(
+        name                =   "jf:flexScale3D",
+        clazz               =   "com.googlecode.jsfFlex.effects.ext.FlexUIScale3D",
+        type                =   "com.googlecode.jsfFlex.FlexUIScale3D",
+        tagClass            =   "com.googlecode.jsfFlex.taglib.effects.ext.FlexUIScale3DTag",
+        family              =   "javax.faces.FlexSimple",
+        defaultRendererType =   "com.googlecode.jsfFlex.FlexScale3D"
+)
+public abstract class AbstractFlexUIScale3D 
+                                extends AbstractFlexUISimpleBase 
+                                implements IFlexUIBaseAttributes {
+    
+    /**
+     * Id of the component.
+     */
+    @JSFProperty(
+            inheritTag  =   true,
+            rtexprvalue =   true,
+            literalOnly =   true,
+            desc        =   "Id of the component."
+    )
+    public String getId(){
+        return super.getId();
+    }
+    
+}
